4a519b6abb5c467ab022f0bc02fc06d94e90904a
Nabil Mutawakkil Qisthi <nabilmq13@gmail.com>
2025-10-18 14:35:48 +0700

    add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl">Page Not Found</h1>
+    </div>
+  );
+}
